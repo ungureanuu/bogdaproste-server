67ff67c173f19ba7ace9fac1d74b9f9f01f91033
Claudiu Ungureanu <claudiu.ungureanu@bearingpoint.com>
2019-05-05 09:30:39 +0300

    added npmrc and db sring

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ let isConnected
 module.exports = async = () => {
     if (isConnected) return Promise.resolve()
 
-    return mongoose.connect(process.env.DB)
+    return mongoose.connect('mongodb+srv://ungureanu:Botosani1989@cluster0-t0frr.mongodb.net/test?retryWrites=true')
         .then(db => {
             isConnected = db.connections[0].readyState
         })
